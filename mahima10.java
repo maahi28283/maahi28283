258cde3c2f778c33d98ab518b967d3b919e14199
maahi28283 <44293861+maahi28283@users.noreply.github.com>
2018-10-21 11:53:33 -0700

    Create mahima10.java

---
+++ mahima10.java
@@ -0,0 +1,16 @@
+public class NumberDigits {
+
+    public static void main(String[] args) {
+
+        int count = 0, num = 12345;
+
+        while(num != 0)
+        {
+            
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println("Number of digits: " + count);
+    }
+}
