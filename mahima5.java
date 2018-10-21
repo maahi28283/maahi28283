fde44dee4043f47c068cfb7fadd25d2bc2ba1fc8
maahi28283 <44293861+maahi28283@users.noreply.github.com>
2018-10-21 11:05:35 -0700

    Create mahima5.java

---
+++ mahima5.java
@@ -0,0 +1,20 @@
+public class mem
+{
+    public static void main(String args[])
+    {
+    int x=1,y=2,z=3;
+ 
+    if(x>y && x>z)
+    {
+        System.out.println("x is largest");
+    }
+    else if (y>z)
+    {
+        System.out.println("y is largest");
+    }
+    else
+    {
+        System.out.println("z is largest");
+    }
+}
+}
