84d309c49286ad57f43d27cf5643170038b3977f
maahi28283 <44293861+maahi28283@users.noreply.github.com>
2018-10-21 10:09:36 -0700

    Update mahima2.java

--- mahima2.java
+++ mahima2.java
@@ -2,8 +2,12 @@ public class evod
 {
  public static void main(String args[])
  {
-   int number=3;
-   if(number%2==0)
+   int number=22212 ;
+   if(number<=0)
+   {
+      System.out.println("invalid");
+   }
+   else if(number%2==0)
    {
      System.out.println(number + "even");
    }
