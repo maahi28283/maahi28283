e52f82166e1f8fc26643d9a9e31313e733cada3e
maahi28283 <44293861+maahi28283@users.noreply.github.com>
2018-10-23 06:35:08 -0400

    Create mahima16.java

---
+++ mahima16.java
@@ -0,0 +1,17 @@
+public class mahi{
+public static void main(String args[]){
+int low=1, high=10,i;
+while(low<high){
+boolean temp=false;
+for(i=2;i<low/2;++i){
+if(low % i==0){
+temp= true;
+break;
+}
+}
+if(!temp)
+System.out.println("" + low);
+++low;
+}
+}
+}
