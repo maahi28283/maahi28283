7781de44525de66291d9a94baf34d75c4a0e6e1c
maahi28283 <44293861+maahi28283@users.noreply.github.com>
2018-11-08 19:50:47 +0530

    Create mahima4.java

---
+++ mahima4.java
@@ -0,0 +1,11 @@
+public class mahi{
+    public static void main(String args[]){
+        char c='U';
+        if((c>='a' && c<='z') || (c>='A' && c<='Z'))
+            System.out.print("yes");
+        else
+            System.out.print("no");
+        
+        
+    }
+}
