63038d6f747fb27399250f446b0b8c4149a3502f
maahi28283 <44293861+maahi28283@users.noreply.github.com>
2018-11-08 19:00:42 +0530

    Create mahima20.java

---
+++ mahima20.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+     int n=3,i,m;
+     
+     for(i=1;i<=5;i++){
+           m=n*i;
+          System.out.println("multiples are "+  m);
+     }
+     }
+}
