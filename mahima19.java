4a186026d63657dfb1253dc3f8859a504d219b8b
maahi28283 <44293861+maahi28283@users.noreply.github.com>
2018-11-08 18:32:47 +0530

    Create mahima19.java

---
+++ mahima19.java
@@ -0,0 +1,12 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+     int n=3,i;
+     int result=1;
+     for(i=1;i<=n;i++)
+     {
+         result = result * i;
+     }
+     System.out.print("result is "+  result);
+     }
+}
